77a8292c98b78b9a4931e6d93bab10c26e94c30a
catherinehorne144 <catherinehorne144@gmail.com>
2025-12-19 14:11:32 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://kssv-ngo.vercel.app"
+  const baseUrl = "https://karungussv.vercel.app"
 
   return {
     rules: [
